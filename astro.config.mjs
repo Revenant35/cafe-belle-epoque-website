34b6462ebf9d8552596c13a8211f542bd603cf1d
Zach Brown <bbrown64506@gmail.com>
2026-08-13 16:14:10 -0500

    fix it again

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://revenant35.github.io',
-  base: 'cafe-belle-epoque-website',
+  base: '/cafe-belle-epoque-website/',
 });
