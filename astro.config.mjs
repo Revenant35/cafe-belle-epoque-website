e41ef5b570152fe5f24a743cd69ce0996b67fbe4
Zach Brown <bbrown64506@gmail.com>
2026-08-13 16:06:50 -0500

    feat: github pages config (take 1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://revenant35.github.io',
+  base: 'cafe-belle-epoque-website',
+});
